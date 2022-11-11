dfdd9d91f3ad576a6f9cbea331aa4b6dd48bba8d
Ritik K. Pillai <102801379+iKoNFlair@users.noreply.github.com>
2022-11-11 20:39:45 +0530

    Day 4 assignment submitted

---
+++ divisibility.java
@@ -0,0 +1,18 @@
+public class divisibility {
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.print("Numbers divisible by 23 between 10 to 150 are : ");
+		
+		for(int i=10; i<=150; i++)
+		{
+			if(i%23==0)
+				{
+				   System.out.print(i + " ");
+				}
+			    
+		}
+	}
+
+}
